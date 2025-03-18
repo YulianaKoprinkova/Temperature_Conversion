e7b041d1583618fa95766c762f303d8fe3d233f5
YulianaKoprinkova <yuliana.koprinkova@gmail.com>
2025-03-18 22:19:17 +0200

    calculations ready

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int temperatureValue;
-        String temperature;
         System.out.println("Please enter the temperature t= ");
         Scanner scanner = new Scanner(System.in);
-        temperatureValue = scanner.nextInt();
+        int temperatureValue = scanner.nextInt();
+        System.out.println("Please enter 1 for conversion Celsius - Fahrenheit or 2 for conversion Fahrenheit - Celsius: ");
+        int temperature = scanner.nextInt();
+        if (temperature == 1) System.out.println("The Fahrenheit temperature is: " + (temperatureValue * 9 / 5 + 32));
+        else System.out.println("The Celsius temperature is: " + ((temperatureValue - 32)*5/9));
 
     }
 }
